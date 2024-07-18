a08ffd169ba12860f155088b97fd33af6beebf94
Esilff <vbanari02@gmail.com>
2024-07-18 19:48:40 +0200

    Adding cell creation at mouse input

--- GenerationParticules/lenia/Lenia.gd
+++ GenerationParticules/lenia/Lenia.gd
@@ -17,27 +17,56 @@ var compute_list : int
 
 var pass_texture : RID
 
+var mouse_pos = [-1,-1]
+var mouse_clicked: bool = false
+
 func _ready():
 	sprite.texture = ImageTexture.create_from_image(create_image(image_width, image_height))
 	shader = load_shader("res://lenia/Lenia.glsl")
 
+
+
 	pass_texture = get_texture_rid(sprite.texture)
-	var uniform_set = rd.uniform_set_create([get_texture_uniform(pass_texture, 0)], shader, 0)
+	
+
+
 	# var uniform_set = rd.uniform_set_create([get_texture_uniform(sprite.texture, 0)], shader, 0)
 
 	pipeline = rd.compute_pipeline_create(shader)
+	
+	rd.submit()
+	rd.sync()
+
+	# var output_texture = rd.texture_get_data()
+	
+
+func _process(_delta):
+	if mouse_clicked:
+		var local_pos = sprite.get_local_mouse_position()
+		local_pos.x += ceil(sprite.texture.get_width() / 2)
+		local_pos.y += ceil(sprite.texture.get_height() / 2)
+		mouse_pos = [local_pos.x, local_pos.y]
+	else:
+		mouse_pos = [-1,-1]
+	var uniform_set = rd.uniform_set_create([get_texture_uniform(pass_texture, 0),get_mouse_pos_buffer_uniform(1)], shader, 0)
 	compute_list = rd.compute_list_begin()
 	rd.compute_list_bind_compute_pipeline(compute_list, pipeline)
 	rd.compute_list_bind_uniform_set(compute_list, uniform_set, 0)
 	rd.compute_list_dispatch(compute_list, ceil(image_width / 16.0),ceil(image_height / 16.0),1)
 	rd.compute_list_end()
-	rd.submit()
-	rd.sync()
 
-	# var output_texture = rd.texture_get_data()
 	var byte_data : PackedByteArray = rd.texture_get_data(pass_texture, 0)
 	var raw_image : Image = Image.create_from_data(image_width, image_height, false, Image.FORMAT_RGBAF, byte_data)
 	sprite.texture = ImageTexture.create_from_image(raw_image)
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			mouse_clicked = true
+		else:
+			mouse_clicked = false
+
+
 
 func load_shader(path: String) -> RID:
 	var shader_file : Resource = load(path)
@@ -66,3 +95,17 @@ func get_texture_uniform(texture: RID, binding: int):
 	output_tex_uniform.binding = binding
 	output_tex_uniform.add_id(texture)
 	return output_tex_uniform
+
+func get_mouse_pos_buffer_uniform(binding: int):
+	var input_data: PackedFloat32Array = PackedFloat32Array(mouse_pos)
+	var input_bytes : PackedByteArray = input_data.to_byte_array()
+
+	var buffer: RID = rd.storage_buffer_create(input_bytes.size(), input_bytes)
+	
+	var uniform : RDUniform = RDUniform.new()
+
+	uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
+	uniform.binding = binding
+	uniform.add_id(buffer)
+	return uniform
+
--- GenerationParticules/lenia/Lenia.glsl
+++ GenerationParticules/lenia/Lenia.glsl
@@ -5,8 +5,15 @@ layout(local_size_x = 16, local_size_y = 16, local_size_z = 1) in;
 
 layout (set = 0, binding = 0, rgba32f) uniform image2D inTex;
 
+layout (set = 0, binding = 1) restrict buffer MouseInfoBuffer {
+    float data[];
+} mouse_info_buffer;
+
 void main() {
-    vec4 color = vec4(1.0,0.0,0.0,1.0);
-    ivec2 texel = ivec2(gl_GlobalInvocationID.xy);
-    imageStore(inTex, texel, color);
+    if (mouse_info_buffer.data[0] > 0.0 && mouse_info_buffer.data[1] > 0.0) {
+        vec4 color = vec4(1.0,0.0,0.0,1.0);
+        ivec2 texel = ivec2(mouse_info_buffer.data[0], mouse_info_buffer.data[1]);
+        imageStore(inTex, texel, color);
+    }
+    
 }
